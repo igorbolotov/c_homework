5b1ca8cdf99126ee2eb95fc26efc3f0b9266c182
igorbolotov <igor.bolotov@me.com>
2023-09-21 23:30:31 +0300

    homework c#

---
+++ HW#3/Program.cs
@@ -0,0 +1,14 @@
+﻿// ДЗ№3
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($"Число {a} четное");
+}
+
+else
+{
+    Console.WriteLine($"Число {a} нечетное");
+}
---
+++ HW#4/Program.cs
@@ -0,0 +1,10 @@
+﻿// ДЗ№4
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 2;
+while (number <= N)
+{
+    Console.Write($"{number}, ");
+    number += 2;
+}
